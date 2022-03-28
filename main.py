1e8203dad2f10cf5751a2e51711c53cc8fb2e442
Sohail <sohjaidi@gmail.com>
2022-03-28 17:09:00 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import lxml.etree
+import lxml.builder
+import csv
+
+def get_level_and_sequence() :
+    return
+
+
+def generate_bs_report_line(element):
+    pass
+
+
+if __name__ == '__main__':
+    pass
